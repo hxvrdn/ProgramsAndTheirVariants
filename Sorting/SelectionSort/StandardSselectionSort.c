f1a079e244d0e063cb807271f61b90e2b5589908
Harshavardhan Asode <hxvrdn@gmail.com>
2026-05-01 21:40:59 +0530

    Standard Selection Sort Algorithm

---
+++ Sorting/SelectionSort/StandardSselectionSort.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+
+/*
+Working:
+- Repeatedly find the minimum element from unsorted part
+- Swap it with the first unsorted position
+
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+*/
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min])
+                min = j;
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array:\n");
+    printArray(arr, n);
+
+    return 0;
+}
